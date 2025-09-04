88a409ac76de2341335583eab27dadcc37ca663d
Daisy <detcherry@gmail.com>
2025-09-04 11:23:30 +0200

    add counter

--- src/components/VisitorCounter/VisitorCounter.jsx
+++ src/components/VisitorCounter/VisitorCounter.jsx
@@ -1,52 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./VisitorCounter.scss";
 
-const COUNTER_KEY = "foxcodeur-livre-jeu"; // Change ce nom si tu veux un compteur distinct
-
 function VisitorCounter() {
-  const [visits, setVisits] = useState(null);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    fetch(`https://api.tallycount.app/increment?key=${COUNTER_KEY}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (typeof data.count === "number") {
-          setVisits(data.count);
-        } else {
-          setError(true);
-        }
-      })
-      .catch(() => setError(true));
-  }, []);
-
-  if (error) {
-    // Affiche l'image SVG si le compteur est indisponible
-    return (
-      <div className="visitor-counter">
-        <img
-          src="/compteur-indisponible.svg"
-          alt="Compteur indisponible"
-          className="visitor-counter__image"
-          width={350}
-          height={50}
-        />
-      </div>
-    );
-  }
-
   return (
     <div className="visitor-counter">
-      <span className="visitor-counter__icon" role="img" aria-label="👥">
-        👥
-      </span>
-      {visits === null ? (
-        <span className="visitor-counter__text">Chargement…</span>
-      ) : (
-        <span className="visitor-counter__text">
-          {visits} joueurs ont tenté l’aventure&nbsp;!
-        </span>
-      )}
+      <img
+        src="https://visitor-badge.glitch.me/badge?page_id=foxcodeur-livre-jeu"
+        alt="Compteur de visiteurs"
+        className="visitor-counter__badge"
+      />
     </div>
   );
 }

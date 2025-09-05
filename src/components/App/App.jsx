1001c15671043b91c50080c1ff221d29c8056bd5
Daisy <detcherry@gmail.com>
2025-09-05 11:04:19 +0200

    modif logo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import routesConfig from "../../config/routesConfig"; // Assurez-vous que ce chemin est correct
+import Footer from "../Footer/Footer"; // Assurez-vous que ce chemin est correct
 import "./App.scss";
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <Routes>
         {routesConfig.map((route, index) => (
           <Route key={index} path={route.path} element={<route.element />} />
         ))}
       </Routes>
+      <Footer />
     </div>
   );
 };

e604eb90077b3e669bf31707df74a4cfb310eff0
Daisy <detcherry@gmail.com>
2025-09-05 14:48:18 +0200

    add scrollToTop

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import routesConfig from "../../config/routesConfig";
 import Footer from "../Footer/Footer";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import ScrollToTopButton from "../ScrollToTopButton/ScrollToTopButton";
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="app-container">
+      <ScrollToTop /> {/* Scroll auto à chaque navigation */}
       <Routes>
         {routesConfig.map((route, index) => (
           <Route key={index} path={route.path} element={<route.element />} />
         ))}
       </Routes>
+      <ScrollToTopButton /> {/* Bouton flottant pour l'utilisateur */}
       <Footer />
     </div>
   );
